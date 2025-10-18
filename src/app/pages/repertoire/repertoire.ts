1811bddaf95259f6c4b271882a60d1c01636644b
Ivana Maricic <ivana.maricic.40.20@ict.edu.rs>
2025-10-18 17:45:29 +0200

    Switched to pipes, added resolver

--- src/app/pages/repertoire/repertoire.ts
+++ src/app/pages/repertoire/repertoire.ts
@@ -2,26 +2,19 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MockDataService } from '../../core/services/mock-data.service';
 import { RouterModule } from '@angular/router';
+import { ProjectionsPipe } from '../../core/pipes/projections.pipe';
+import { MovieByIdPipe } from '../../core/pipes/movieById.pipe';
 
 @Component({
   selector: 'app-repertoire',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ProjectionsPipe,MovieByIdPipe],
   templateUrl: './repertoire.html',
   styleUrls: ['./repertoire.css']
 })
 
 export class Repertoire {
   private mockDataService = inject(MockDataService);
-  readonly repertoire = this.mockDataService.repertoire;
-
-
-  getMovie(id: number) {
-  return this.mockDataService.getMovieById(id);
-  }
-
-  getProjections(movieId: number, repertoireId: number) {
-    return this.mockDataService.getProjectionsByMovieAndRepertoire(movieId, repertoireId) || [];
-  }
-
+  repertoire$ = this.mockDataService.getAllRepertoire();
+  movies$ = this.mockDataService.getAllMovies();
 }
